e539e4fa2f1dc35e0a376a1e3255d47bf32ca694
Dani Zhang <dzhang@dibal.com>
2023-08-31 13:17:38 +0200

    Archivos

---
+++ WindowsFormsApp1/Archivos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Archivos : Form
+    {
+        public Archivos()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NetworkCredential nc = new NetworkCredential(@"CS1200", "CS1200");
+            CredentialCache cc = new CredentialCache();
+            cc.Add(new Uri(@"\\192.168.150.66"), "Basic", nc);
+            string[] f = Directory.GetDirectories(@"\\192.168.150.66\c$\SW1100");
+        }
+    }
+}
